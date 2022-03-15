1fcf8f8dc89ed079052ce34941544ee8ec421aa8
juhyoungryu <thinkoutside2004@gmail.com>
2022-03-15 18:33:24 +0900

    클론코딩 3.10

--- src/server.js
+++ src/server.js
@@ -2,16 +2,33 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const handleHome = (req, res) => {
-  return res.send("I still love you.");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
 };
 
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
+  next();
 };
+
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
+
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
 
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListenting = () => {
   console.log(`☑️  Server listening on port http://localhost:${PORT} 🚀`);
