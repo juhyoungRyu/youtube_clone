6b74731b5c78bbb59b0436d97e366ac545e64655
bobready0810 <daeheonoff@gmail.com>
2022-03-17 12:11:09 +0900

    클론코딩 5.3

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 export const trending = (req, res) => res.render("home");
 
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.send("Edit Vedio");
+export const edit = (req, res) => res.render("edit")
 
 export const search = (req, res) => res.send("Search");
 
