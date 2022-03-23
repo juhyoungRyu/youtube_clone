6d9534818defd31a2b998c41a67be08674991d0b
juhyoungryu <thinkoutside2004@gmail.com>
2022-03-23 14:04:51 +0900

    클론코딩 #6.21 끝

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -22,11 +22,17 @@ export const getEdit = async (req, res) => {
     return res.render("404", { pageTitle: "Video not found." });
   }
 
-  return res.render("edit", { pageTitle: `Editing`, video });
+  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
 export const postEdit = (req, res) => {
   const { id } = req.params;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+
   return res.redirect(`/videos/${id}`);
 };
 
