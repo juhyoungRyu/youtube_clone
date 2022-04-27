fa6db43b257c5a0846df37a9716594218133fec6
juhyoungryu <thinkoutside2004@gmail.com>
2022-04-27 23:48:41 +0900

    비밀번호 해싱

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,12 +2,11 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 
-
-export const postJoin = (req, res) => {
-
 export const postJoin = async (req, res) => {
   console.log(req.body);
+
   const { name, username, email, password, location } = req.body;
+
   await User.create({
     name,
     username,
@@ -15,8 +14,10 @@ export const postJoin = async (req, res) => {
     password,
     location,
   });
+
   return res.redirect("/login");
 };
+
 export const edit = (req, res) => res.send("Edit User");
 
 export const remove = (req, res) => res.send("Delete User");
--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
@@ -8,6 +9,9 @@ const userSchema = new mongoose.Schema({
   location: String,
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 5);
+});
 
+const User = mongoose.model("User", userSchema);
 export default User;
