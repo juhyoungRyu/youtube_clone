d375468deff1d71e5377b2bf51cf9bfafa862c90
juhyoungRyu <thinkoutside2004@gmail.com>
2022-05-18 15:10:57 +0900

    회원가입 완료

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,9 +4,28 @@ export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 
 export const postJoin = async (req, res) => {
   console.log(req.body);
-
-  const { name, username, email, password, location } = req.body;
-
+  const { name, username, email, password, password2, location } = req.body;
+  const usernameExists = await User.exists({ username });
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+  if (usernameExists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username is already taken",
+    });
+  }
+  const emailExists = await User.exists({ email });
+  if (emailExists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This email is already taken",
+    });
+  }
   await User.create({
     name,
     username,
