acdb4b75e7e92b7064e2c4b4f68cb93ae42f1bd5
juhyoungRyu <thinkoutside2004@gmail.com>
2022-05-18 15:40:41 +0900

    404 처리:

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,27 +5,24 @@ export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
   console.log(req.body);
   const { name, username, email, password, password2, location } = req.body;
-  const usernameExists = await User.exists({ username });
+
   const pageTitle = "Join";
-  if (password !== password2) {
-    return res.render("join", {
-      pageTitle,
-      errorMessage: "Password confirmation does not match.",
-    });
-  }
-  if (usernameExists) {
-    return res.render("join", {
+
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "This username is already taken",
     });
   }
-  const emailExists = await User.exists({ email });
-  if (emailExists) {
-    return res.render("join", {
+
+  if (password !== password2) {
+    return res.status(400).render("join", {
       pageTitle,
-      errorMessage: "This email is already taken",
+      errorMessage: "Password confirmation does not match.",
     });
   }
+
   await User.create({
     name,
     username,
