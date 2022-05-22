c67047f22125e108028e3c5b43b010b89310e5f2
juhyoungryu <thinkoutside2004@gmail.com>
2022-05-22 15:14:43 +0900

    git Oauth 구현 중

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import bcript from "bcrypt";
+import fetch from "node-fetch";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 
@@ -41,10 +42,6 @@ export const postJoin = async (req, res) => {
   }
 };
 
-export const edit = (req, res) => res.send("Edit User");
-
-export const remove = (req, res) => res.send("Delete User");
-
 export const getLogin = (req, res) => {
   res.render("login", { pageTitle: "Login" });
 };
@@ -72,6 +69,47 @@ export const postLogin = async (req, res) => {
   return res.redirect("/");
 };
 
-export const logout = (req, res) => res.send("Logout");
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
 
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+
+  const params = new URLSearchParams(config).toString();
+
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+
+  const data = await fetch(finalUrl, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  const json = await data.json();
+
+  console.log(json);
+  res.send(JSON.stringify(json));
+};
+
+export const edit = (req, res) => res.send("Edit User");
+export const remove = (req, res) => res.send("Delete User");
+export const logout = (req, res) => res.send("Logout");
 export const see = (req, res) => res.send("see");
